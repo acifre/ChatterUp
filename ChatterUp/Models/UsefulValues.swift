513223c7aef6359b10ff8257e4d75de47bfbcf27
acifre <anthonybcifre@gmail.com>
2023-05-06 23:17:10 -0500

    Trying to fix gitignore

--- ChatterUp/Models/UsefulValues.swift
+++ ChatterUp/Models/UsefulValues.swift
@@ -9,5 +9,5 @@ import Foundation
 
 
 struct UsefulValues {
-    static var apiKey = "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"
+    static var apiKey = "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"
 }
