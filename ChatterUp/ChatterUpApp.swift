66f71f072a49cc27d97494a9623a11d016982e7a
acifre <anthonybcifre@gmail.com>
2023-05-06 20:36:11 -0500

    Initial Commit

---
+++ ChatterUp/ChatterUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatterUpApp.swift
+//  ChatterUp
+//
+//  Created by Anthony Cifre on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatterUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
