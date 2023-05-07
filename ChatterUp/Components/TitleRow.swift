42a8aa4e998c16ce6d5eec501e81a7a9b1a7f8f4
acifre <anthonybcifre@gmail.com>
2023-05-06 20:45:43 -0500

    Add custom colors and TitleRow view

---
+++ ChatterUp/Components/TitleRow.swift
@@ -0,0 +1,20 @@
+//
+//  TitleRow.swift
+//  ChatterUp
+//
+//  Created by Anthony Cifre on 5/6/23.
+//
+
+import SwiftUI
+
+struct TitleRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TitleRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleRow()
+    }
+}
